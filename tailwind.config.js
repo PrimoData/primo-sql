9f64812aee7c3daef3539f23975ec45e22621ef5
Primo Data <mershon.max16@gmail.com>
2023-10-27 09:58:18 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
+  darkMode: false,
   content: [
     'app/**/*.{ts,tsx}',
     'components/**/*.{ts,tsx}',
